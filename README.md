6e00305e1ad63d582099fcaae9354bf27b9aad08
Sanket3212 <sanketkumbhar3212@gmail.com>
2025-07-19 15:42:05 +0530

    configured

--- README.md
+++ README.md
@@ -73,7 +73,7 @@ Follow these instructions to get a copy of the project up and running on your lo
 
 1.  **Clone the repository:**
     ```bash
-    git clone [https://github.com/your-username/resolve.git](https://github.com/your-username/resolve.git)
+    https://github.com/Sanket3212/Resolve.git
     cd resolve
     ```
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,12 +4,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  runtimeConfig: {
-    public: {
-      supabaseUrl:process.env.SUPABASE_URL,
-      supabaseKey: process.env.SUPABASE_KEY
-    }
-  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/supabase'
+  ]
 
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase']
+
 })
